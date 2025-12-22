6a82430cc794e7ec03936390c00dbcbe43c1a868
vhr dashboard <regatpeter@hotmail.fr>
2025-12-23 00:50:31 +0100

    chore: disable auto-launch and rewire shortcut

--- create-desktop-shortcut.ps1
+++ create-desktop-shortcut.ps1
@@ -7,8 +7,8 @@ $Desktop = [Environment]::GetFolderPath("Desktop")
 $ShortcutPath = Join-Path $Desktop "VHR Dashboard Pro.lnk"
 
 $Shortcut = $WshShell.CreateShortcut($ShortcutPath)
-$Shortcut.TargetPath = "wscript.exe"
-$Shortcut.Arguments = "`"C:\Users\peter\VR-Manager\VHR-Dashboard-Invisible.vbs`""
+$Shortcut.TargetPath = "C:\Users\peter\VR-Manager\scripts\launch-dashboard-smart.bat"
+$Shortcut.Arguments = ""
 $Shortcut.WorkingDirectory = "C:\Users\peter\VR-Manager"
 $Shortcut.IconLocation = "C:\Windows\System32\shell32.dll,13"
 $Shortcut.Description = "Lance VHR Dashboard Pro (serveur en arrière-plan)"
